1ee5aed7f56f505adfea15eba89059e17d46ea41
JinHe <18791461386@163.com>
2021-12-03 21:37:08 +0800

    Mongoose model validation.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,14 +8,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   console.log(err);
 });
 
-// const User = mongoose.model('User', {
-//   name: String,
-//   age: Number,
-// });
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate: (val) => {
+      if (val < 0) {
+        throw new Error('Age must be a positive number.');
+      }
+    }
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    validate: (val) => {
+      if (!validator.isEmail(val)) {
+        throw new Error('Email is invalid.');
+      }
+    }
+  },
+  password: {
+    type: String,
+    trim: true,
+    minlength: 7,
+    validate: (val) => {
+      if (val && val.toLowerCase().indexOf('password') > -1) {
+        throw new Error('Password should not contain words like password');
+      }
+    }
+  }
+});
 
 // const me = new User({
-//   name: 'AdobeHJ',
-//   age: 28,
+//   name: 'Tom',
+//   age: 20,
+//   email: 'tom.test@qq.io',
+//   password: 'pass-tom',
 // });
 
 // me.save().then(() => {
@@ -24,17 +59,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 // });
 
 const Task = mongoose.model('Task', {
-  description: String,
-  completed: Boolean,
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const reading = new Task({
-  description: 'Read a book every month.',
-  completed: false,
+const learning = new Task({
+  description: '  Learning nodejs every day.  ',
 });
 
-reading.save().then(() => {
-  console.log(reading);
+learning.save().then(() => {
+  console.log(learning);
 }).catch((e) => {
   console.log(e);
 });
